78041e971141fb94758d172ce4e70978531a812f
Caua <cauaamorim20040413@gmail.com>
2023-01-16 12:14:02 -0300

    some updates

--- scripts/events.js
+++ scripts/events.js
@@ -2,10 +2,12 @@
 
 function redirect(triggerCardClassNameWithoutDot, url) {
     document.querySelector(`.${triggerCardClassNameWithoutDot}`).addEventListener('click', () => {
-        location.href = url;
+        window.open(url, '_blank');
     })
 }
 
 // GitHub
 redirect('first-project', 'https://github.com/C6u6/notifications-service');
 redirect('second-project', 'https://github.com/C6u6/mail');
+redirect('third-project', 'https://github.com/C6u6/student');
+redirect('forth-project', 'https://github.com/C6u6/scientia');
